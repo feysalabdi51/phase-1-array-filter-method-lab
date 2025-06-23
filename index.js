1aa46c923646b0fe4ce37c624e713b5b32705934
feysalabdi51 <pheysalupdyy@gmail.com>
2025-06-23 14:59:24 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+// 1. findMatching: case-insensitive match
+function findMatching(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+
+// 2. fuzzyMatch: match beginning letters
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+// 3. matchName: match exact name in driver objects
+function matchName(drivers, query) {
+  return drivers.filter(driver => driver.name === query);
+}
+
